e1478e0a5c3a4ed4edf561b11d0773a6fcbb9f73
anuraag <anuraag.warudkar@gmail.com>
2024-11-18 16:22:16 -0500

    added line count script

---
+++ lc.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# directories to search
+SRC_DIRS="src include tests examples"
+
+# file extensions to include
+EXTENSIONS="c h"
+
+# find and count lines
+TOTAL=$(find $SRC_DIRS -type f \( -name "*.c" -o -name "*.h" \) -exec wc -l {} + | awk '{sum += $1} END {print sum}')
+echo "total line count: $TOTAL"
