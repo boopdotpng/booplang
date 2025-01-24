8c7bdb5060aef8d3e069ce3df3483d5a0f817607
anuraag <anuraag.warudkar@gmail.com>
2025-01-24 02:59:31 -0500

    fix capacity in vector.c

--- src/vector.c
+++ src/vector.c
@@ -8,7 +8,7 @@ vector *create_vector(size_t elem_size, int initial_size) {
     vector *arr = malloc(sizeof(vector));
     arr->data = malloc(elem_size * initial_size);
     arr->size = 0;
-    arr->capacity = elem_size * initial_size;
+    arr->capacity = initial_size;
     arr->elem_size = elem_size;
 
     return arr;
